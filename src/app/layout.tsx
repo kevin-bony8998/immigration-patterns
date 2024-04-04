f8bb3e41fa1dda0027d443e456e4af176f09c689
unknown <nivekynob@gmail.com>
2024-04-05 01:36:30 +0530

    feat(immigration-patterns): Added zustand store

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,19 +1,16 @@
 "use client";
 
-import type { Metadata } from "next";
-import { Inter } from "next/font/google";
 import "./globals.css";
-// import { World } from "./World";
 import dynamic from 'next/dynamic';
 import { useEffect, useState } from "react";
+import { create } from "zustand";
 
-const inter = Inter({ subsets: ["latin"] });
+const useGlobalDataStore = create((set) => ({
+  selectedTabIdx: 0,
+  setSelectedTabIdx: (idx: number) => set({selectedTabIdx: idx}),
+}))
 
-export default function RootLayout({
-  children,
-}: Readonly<{
-  children: React.ReactNode;
-}>) {
+export default function RootLayout() {
   const World = dynamic(
     () => import('./World').then((module) => module.World),
     {
@@ -27,10 +24,16 @@ export default function RootLayout({
   }, []);
   return (
     <html lang="en">
-      <body className={'main-body-class ' + inter.className}>{children}</body>
-      {domLoaded && (
-        <World />
-      )}
+      <head>
+        <link rel="preconnect" href="https://fonts.googleapis.com" />
+        <link rel="preconnect" href="https://fonts.gstatic.com" />
+        <link href="https://fonts.googleapis.com/css2?family=Raleway:ital,wght@0,100..900;1,100..900&display=swap" rel="stylesheet" />
+      </head>
+      <body className={'main-body-class '}>
+        {domLoaded && (
+          <World />
+        )}
+      </body>
     </html>
   );
 }
