9d53bf8f6eadfe6c21e96b1df6c96b093cf94545
mahita08 <59723054+mahita08@users.noreply.github.com>
2024-04-05 16:07:20 -0400

    Update ImmigrationData2018.ts

--- src/app/datasets/ImmigrationData2018.ts
+++ src/app/datasets/ImmigrationData2018.ts
@@ -8,7 +8,7 @@ export const labelsValues_2018 = {
                 "latitude": "26.820553",
                 "longitude": "30.802498",
                 "pop_max": 13000000,
-                "labelColour": "red"
+                "labelColour": "rgba(255, 18, 19, 0.2)"
             }
         },
         {
@@ -18,7 +18,7 @@ export const labelsValues_2018 = {
                 "latitude": "31.791702",
                 "longitude": "-7.092620",
                 "pop_max": 12900000,
-                "labelColour": "red"
+                "labelColour": "rgba(255, 18, 19, 0.2)"
             }
         },
         {
@@ -28,7 +28,7 @@ export const labelsValues_2018 = {
                 "latitude": "-30.559483",
                 "longitude": "22.937506",
                 "pop_max": 10200000,
-                "labelColour": "red"
+                "labelColour": "rgba(255, 18, 19, 0.2)"
             }
         },
         {
@@ -38,7 +38,7 @@ export const labelsValues_2018 = {
                 "latitude": "33.886917",
                 "longitude": "9.537499",
                 "pop_max": 9400000,
-                "labelColour": "red"
+                "labelColour": "rgba(255, 18, 19, 0.2)"
             }
         },
         {
@@ -48,7 +48,7 @@ export const labelsValues_2018 = {
                 "latitude": "28.033886",
                 "longitude": "1.659626",
                 "pop_max": 2400000,
-                "labelColour": "red"
+                "labelColour": "rgba(255, 18, 19, 0.2)"
             }
         },
         {
@@ -58,7 +58,7 @@ export const labelsValues_2018 = {
                 "latitude": "-18.4554963",
                 "longitude": "29.7468414",
                 "pop_max": 2300000,
-                "labelColour": "red"
+                "labelColour": "rgba(255, 18, 19, 0.2)"
             }
         },
         {
@@ -68,7 +68,7 @@ export const labelsValues_2018 = {
                 "latitude": "-19.302233",
                 "longitude": "34.9144977",
                 "pop_max": 2000000,
-                "labelColour": "red"
+                "labelColour": "rgba(255, 18, 19, 0.2)"
             }
         },
         {
@@ -78,7 +78,7 @@ export const labelsValues_2018 = {
                 "latitude": "7.9897371",
                 "longitude": "-5.5679458",
                 "pop_max": 0,
-                "labelColour": "red"
+                "labelColour": "rgba(255, 18, 19, 0.2)"
             }
         },
         {
@@ -88,7 +88,7 @@ export const labelsValues_2018 = {
                 "latitude": "1.4419683",
                 "longitude": "38.4313975",
                 "pop_max": 0,
-                "labelColour": "red"
+                "labelColour": "rgba(255, 18, 19, 0.2)"
             }
         },
         {
@@ -98,7 +98,7 @@ export const labelsValues_2018 = {
                 "latitude": "-23.1681782",
                 "longitude": "24.5928742",
                 "pop_max": 0,
-                "labelColour": "red"
+                "labelColour": "rgba(255, 18, 19, 0.2)"
             }
         },
         {
@@ -108,7 +108,7 @@ export const labelsValues_2018 = {
                 "latitude": "39.7837304",
                 "longitude": "-100.445882",
                 "pop_max": 79300000,
-                "labelColour": "red"
+                "labelColour": "rgba(255, 18, 19, 0.2)"
             }
         },
         {
@@ -118,7 +118,7 @@ export const labelsValues_2018 = {
                 "latitude": "23.6585116",
                 "longitude": "-102.0077097",
                 "pop_max": 45000000,
-                "labelColour": "red"
+                "labelColour": "rgba(255, 18, 19, 0.2)"
             }
         },
         {
@@ -128,7 +128,7 @@ export const labelsValues_2018 = {
                 "latitude": "61.0666922",
                 "longitude": "-107.991707",
                 "pop_max": 22100000,
-                "labelColour": "red"
+                "labelColour": "rgba(255, 18, 19, 0.2)"
             }
         },
         {
@@ -138,7 +138,7 @@ export const labelsValues_2018 = {
                 "latitude": "-34.9964963",
                 "longitude": "-64.9672817",
                 "pop_max": 7400000,
-                "labelColour": "red"
+                "labelColour": "rgba(255, 18, 19, 0.2)"
             }
         },
         {
@@ -148,7 +148,7 @@ export const labelsValues_2018 = {
                 "latitude": "19.0974031",
                 "longitude": "-70.3028026",
                 "pop_max": 6400000,
-                "labelColour": "red"
+                "labelColour": "rgba(255, 18, 19, 0.2)"
             }
         },
         {
@@ -158,7 +158,7 @@ export const labelsValues_2018 = {
                 "latitude": "-10.3333333",
                 "longitude": "-53.2",
                 "pop_max": 6400000,
-                "labelColour": "red"
+                "labelColour": "rgba(255, 18, 19, 0.2)"
             }
         },
         {
@@ -168,7 +168,7 @@ export const labelsValues_2018 = {
                 "latitude": "-6.8699697",
                 "longitude": "-75.0458515",
                 "pop_max": 5200000,
-                "labelColour": "red"
+                "labelColour": "rgba(255, 18, 19, 0.2)"
             }
         },
         {
@@ -178,7 +178,7 @@ export const labelsValues_2018 = {
                 "latitude": "-31.7613365",
                 "longitude": "-71.3187697",
                 "pop_max": 4500000,
-                "labelColour": "red"
+                "labelColour": "rgba(255, 18, 19, 0.2)"
             }
         },
         {
@@ -188,7 +188,7 @@ export const labelsValues_2018 = {
                 "latitude": "23.0131338",
                 "longitude": "-80.8328748",
                 "pop_max": 4300000,
-                "labelColour": "red"
+                "labelColour": "rgba(255, 18, 19, 0.2)"
             }
         },
         {
@@ -198,7 +198,7 @@ export const labelsValues_2018 = {
                 "latitude": "4.099917",
                 "longitude": "-72.9088133",
                 "pop_max": 4200000,
-                "labelColour": "red"
+                "labelColour": "rgba(255, 18, 19, 0.2)"
             }
         },
         {
@@ -208,7 +208,7 @@ export const labelsValues_2018 = {
                 "latitude": "35.000074",
                 "longitude": "104.999927",
                 "pop_max": 65700000,
-                "labelColour": "red"
+                "labelColour": "rgba(255, 18, 19, 0.2)"
             }
         },
         {
@@ -218,7 +218,7 @@ export const labelsValues_2018 = {
                 "latitude": "14.8971921",
                 "longitude": "100.83273",
                 "pop_max": 39800000,
-                "labelColour": "red"
+                "labelColour": "rgba(255, 18, 19, 0.2)"
             }
         },
         {
@@ -228,7 +228,7 @@ export const labelsValues_2018 = {
                 "latitude": "36.5748441",
                 "longitude": "139.2394179",
                 "pop_max": 32200000.000000004,
-                "labelColour": "red"
+                "labelColour": "rgba(255, 18, 19, 0.2)"
             }
         },
         {
@@ -238,7 +238,7 @@ export const labelsValues_2018 = {
                 "latitude": "4.5693754",
                 "longitude": "102.2656823",
                 "pop_max": 26100000,
-                "labelColour": "red"
+                "labelColour": "rgba(255, 18, 19, 0.2)"
             }
         },
         {
@@ -248,7 +248,7 @@ export const labelsValues_2018 = {
                 "latitude": "22.350627",
                 "longitude": "114.1849161",
                 "pop_max": 23800000,
-                "labelColour": "red"
+                "labelColour": "rgba(255, 18, 19, 0.2)"
             }
         },
         {
@@ -258,7 +258,7 @@ export const labelsValues_2018 = {
                 "latitude": "22.1899448",
                 "longitude": "113.5380454",
                 "pop_max": 18600000,
-                "labelColour": "red"
+                "labelColour": "rgba(255, 18, 19, 0.2)"
             }
         },
         {
@@ -268,7 +268,7 @@ export const labelsValues_2018 = {
                 "latitude": "15.9266657",
                 "longitude": "107.9650855",
                 "pop_max": 18000000,
-                "labelColour": "red"
+                "labelColour": "rgba(255, 18, 19, 0.2)"
             }
         },
         {
@@ -278,7 +278,7 @@ export const labelsValues_2018 = {
                 "latitude": "22.3511148",
                 "longitude": "78.6677428",
                 "pop_max": 17900000,
-                "labelColour": "red"
+                "labelColour": "rgba(255, 18, 19, 0.2)"
             }
         },
         {
@@ -288,7 +288,7 @@ export const labelsValues_2018 = {
                 "latitude": "36.638392",
                 "longitude": "127.6961188",
                 "pop_max": 17500000,
-                "labelColour": "red"
+                "labelColour": "rgba(255, 18, 19, 0.2)"
             }
         },
         {
@@ -298,7 +298,7 @@ export const labelsValues_2018 = {
                 "latitude": "-2.4833826",
                 "longitude": "117.8902853",
                 "pop_max": 15500000,
-                "labelColour": "red"
+                "labelColour": "rgba(255, 18, 19, 0.2)"
             }
         },
         {
@@ -308,7 +308,7 @@ export const labelsValues_2018 = {
                 "latitude": "46.603354",
                 "longitude": "1.8883335",
                 "pop_max": 90900000,
-                "labelColour": "red"
+                "labelColour": "rgba(255, 18, 19, 0.2)"
             }
         },
         {
@@ -318,7 +318,7 @@ export const labelsValues_2018 = {
                 "latitude": "39.3260685",
                 "longitude": "-4.8379791",
                 "pop_max": 83500000,
-                "labelColour": "red"
+                "labelColour": "rgba(255, 18, 19, 0.2)"
             }
         },
         {
@@ -328,7 +328,7 @@ export const labelsValues_2018 = {
                 "latitude": "38.9597594",
                 "longitude": "34.9249653",
                 "pop_max": 51200000,
-                "labelColour": "red"
+                "labelColour": "rgba(255, 18, 19, 0.2)"
             }
         },
         {
@@ -338,7 +338,7 @@ export const labelsValues_2018 = {
                 "latitude": "42.6384261",
                 "longitude": "12.674297",
                 "pop_max": 64500000,
-                "labelColour": "red"
+                "labelColour": "rgba(255, 18, 19, 0.2)"
             }
         },
         {
@@ -348,7 +348,7 @@ export const labelsValues_2018 = {
                 "latitude": "54.7023545",
                 "longitude": "-3.2765753",
                 "pop_max": 39400000,
-                "labelColour": "red"
+                "labelColour": "rgba(255, 18, 19, 0.2)"
             }
         },
         {
@@ -358,7 +358,7 @@ export const labelsValues_2018 = {
                 "latitude": "51.1638175",
                 "longitude": "10.4478313",
                 "pop_max": 39600000,
-                "labelColour": "red"
+                "labelColour": "rgba(255, 18, 19, 0.2)"
             }
         },
         {
@@ -368,7 +368,7 @@ export const labelsValues_2018 = {
                 "latitude": "38.9953683",
                 "longitude": "21.9877132",
                 "pop_max": 31300000,
-                "labelColour": "red"
+                "labelColour": "rgba(255, 18, 19, 0.2)"
             }
         },
         {
@@ -378,7 +378,7 @@ export const labelsValues_2018 = {
                 "latitude": "47.59397",
                 "longitude": "14.12456",
                 "pop_max": 31900000,
-                "labelColour": "red"
+                "labelColour": "rgba(255, 18, 19, 0.2)"
             }
         },
         {
@@ -388,7 +388,7 @@ export const labelsValues_2018 = {
                 "latitude": "39.6621648",
                 "longitude": "-8.1353519",
                 "pop_max": 24500000,
-                "labelColour": "red"
+                "labelColour": "rgba(255, 18, 19, 0.2)"
             }
         },
         {
@@ -398,7 +398,7 @@ export const labelsValues_2018 = {
                 "latitude": "52.2434979",
                 "longitude": "5.6343227",
                 "pop_max": 20100000,
-                "labelColour": "red"
+                "labelColour": "rgba(255, 18, 19, 0.2)"
             }
         },
         {
@@ -408,7 +408,7 @@ export const labelsValues_2018 = {
                 "latitude": "25.6242618",
                 "longitude": "42.3528328",
                 "pop_max": 17500000,
-                "labelColour": "red"
+                "labelColour": "rgba(255, 18, 19, 0.2)"
             }
         },
         {
@@ -418,7 +418,7 @@ export const labelsValues_2018 = {
                 "latitude": "24.0002488",
                 "longitude": "53.9994829",
                 "pop_max": 16700000,
-                "labelColour": "red"
+                "labelColour": "rgba(255, 18, 19, 0.2)"
             }
         },
         {
@@ -428,7 +428,7 @@ export const labelsValues_2018 = {
                 "latitude": "32.6475314",
                 "longitude": "54.5643516",
                 "pop_max": 9100000,
-                "labelColour": "red"
+                "labelColour": "rgba(255, 18, 19, 0.2)"
             }
         },
         {
@@ -438,7 +438,7 @@ export const labelsValues_2018 = {
                 "latitude": "30.8124247",
                 "longitude": "34.8594762",
                 "pop_max": 4600000,
-                "labelColour": "red"
+                "labelColour": "rgba(255, 18, 19, 0.2)"
             }
         },
         {
@@ -448,7 +448,7 @@ export const labelsValues_2018 = {
                 "latitude": "31.1667049",
                 "longitude": "36.941628",
                 "pop_max": 4500000,
-                "labelColour": "red"
+                "labelColour": "rgba(255, 18, 19, 0.2)"
             }
         },
         {
@@ -458,7 +458,7 @@ export const labelsValues_2018 = {
                 "latitude": "26.1551249",
                 "longitude": "50.5344606",
                 "pop_max": 3800000,
-                "labelColour": "red"
+                "labelColour": "rgba(255, 18, 19, 0.2)"
             }
         },
         {
@@ -468,7 +468,7 @@ export const labelsValues_2018 = {
                 "latitude": "21.0000287",
                 "longitude": "57.0036901",
                 "pop_max": 2500000,
-                "labelColour": "red"
+                "labelColour": "rgba(255, 18, 19, 0.2)"
             }
         },
         {
@@ -478,7 +478,7 @@ export const labelsValues_2018 = {
                 "latitude": "25.3336984",
                 "longitude": "51.2295295",
                 "pop_max": 2100000,
-                "labelColour": "red"
+                "labelColour": "rgba(255, 18, 19, 0.2)"
             }
         },
         {
@@ -488,7 +488,7 @@ export const labelsValues_2018 = {
                 "latitude": "33.8750629",
                 "longitude": "35.843409",
                 "pop_max": 1900000,
-                "labelColour": "red"
+                "labelColour": "rgba(255, 18, 19, 0.2)"
             }
         }
     ],
