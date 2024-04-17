f8c9a8929dc995fd4241f76a22a8c9c9bf6dcc59
mahita08 <59723054+mahita08@users.noreply.github.com>
2024-04-17 16:39:25 -0400

    hopefully-the-last-one

--- src/app/helperUtils/helper.tsx
+++ src/app/helperUtils/helper.tsx
@@ -2,13 +2,6 @@ import { labelsValues_2018 } from "../datasets/ImmigrationData2018";
 import { labelsValues_2019 } from "../datasets/ImmigrationData2019";
 import { labelsValues_2020 } from "../datasets/ImmigrationData2020";
 
-const mergeDatasets = () => {
-    const resultantJSON = labelsValues_2018;
-    labelsValues_2019.features.forEach((datapoint: any) => {
-        resultantJSON.features.push(datapoint);
-    })
-    return resultantJSON;
-}
 
 export const returnDataToBeUsed = (selectedIdx: number) => {
     if (selectedIdx === 0) {
@@ -18,9 +11,6 @@ export const returnDataToBeUsed = (selectedIdx: number) => {
         return labelsValues_2019;
     }
     else if (selectedIdx === 2) {
-        return mergeDatasets();
-    }
-    else if (selectedIdx === 3) {
         return labelsValues_2020;
     }
 }
