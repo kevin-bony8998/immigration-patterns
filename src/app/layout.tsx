f178548c70c5c95322240eeba3aac75b75b9d7af
unknown <nivekynob@gmail.com>
2024-04-18 01:49:32 +0530

    Removed helloooo line

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -20,7 +20,6 @@ export default function RootLayout() {
         <link href="https://fonts.googleapis.com/css2?family=Raleway:ital,wght@0,100..900;1,100..900&display=swap" rel="stylesheet" />
       </head>
       <body className={'main-body-class '}>
-        <div className="page-title"> Helloooooo </div>
           <Tabs />
           <WorldWrapperComp />
       </body>
